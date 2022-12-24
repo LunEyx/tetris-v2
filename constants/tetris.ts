ed89bdd23fdd8837d0a7d279cbde427f8061fa8c
LunEyx <luneyxlab@gmail.com>
2022-12-25 00:40:24 +0800

    Update available block constant

--- constants/tetris.ts
+++ constants/tetris.ts
@@ -10,4 +10,40 @@ export const BLOCKS = [
     { x: 2, y: 0, color: TetrisBlockColor.CYAN },
     { x: 3, y: 0, color: TetrisBlockColor.CYAN },
   ],
+  [
+    { x: 0, y: 0, color: TetrisBlockColor.BLUE },
+    { x: 0, y: 1, color: TetrisBlockColor.BLUE },
+    { x: 1, y: 1, color: TetrisBlockColor.BLUE },
+    { x: 2, y: 1, color: TetrisBlockColor.BLUE },
+  ],
+  [
+    { x: 2, y: 0, color: TetrisBlockColor.ORANGE },
+    { x: 1, y: 1, color: TetrisBlockColor.ORANGE },
+    { x: 2, y: 1, color: TetrisBlockColor.ORANGE },
+    { x: 0, y: 1, color: TetrisBlockColor.ORANGE },
+  ],
+  [
+    { x: 0, y: 0, color: TetrisBlockColor.YELLOW },
+    { x: 1, y: 0, color: TetrisBlockColor.YELLOW },
+    { x: 1, y: 1, color: TetrisBlockColor.YELLOW },
+    { x: 0, y: 1, color: TetrisBlockColor.YELLOW },
+  ],
+  [
+    { x: 0, y: 1, color: TetrisBlockColor.GREEN },
+    { x: 1, y: 1, color: TetrisBlockColor.GREEN },
+    { x: 1, y: 0, color: TetrisBlockColor.GREEN },
+    { x: 2, y: 0, color: TetrisBlockColor.GREEN },
+  ],
+  [
+    { x: 0, y: 1, color: TetrisBlockColor.PURPLE },
+    { x: 1, y: 1, color: TetrisBlockColor.PURPLE },
+    { x: 1, y: 0, color: TetrisBlockColor.PURPLE },
+    { x: 2, y: 1, color: TetrisBlockColor.PURPLE },
+  ],
+  [
+    { x: 0, y: 0, color: TetrisBlockColor.RED },
+    { x: 1, y: 0, color: TetrisBlockColor.RED },
+    { x: 1, y: 1, color: TetrisBlockColor.RED },
+    { x: 2, y: 1, color: TetrisBlockColor.RED },
+  ],
 ]
